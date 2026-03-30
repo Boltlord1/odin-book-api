21339ee808c5fc2e3f4cc81369756d91d7c9238e
Boltlord1 <boltlord1@gmail.com>
2026-03-30 11:37:01 +0500

    feat: Add script to generate private and public keys for jwt

---
+++ scripts/key.js
@@ -0,0 +1,27 @@
+import { generateKeyPair } from 'node:crypto'
+import fs from 'node:fs'
+
+generateKeyPair(
+	'rsa',
+	{
+		modulusLength: 4096,
+		publicKeyEncoding: {
+			type: 'spki',
+			format: 'pem'
+		},
+		privateKeyEncoding: {
+			type: 'pkcs8',
+			format: 'pem'
+		}
+	},
+	(err, publicKey, privateKey) => {
+		if (err) {
+			console.log(err)
+			return
+		}
+
+		const path = `${import.meta.dirname.slice(0, -7)}generated/`
+		fs.writeFileSync(`${path}public.pem`, publicKey)
+		fs.writeFileSync(`${path}private.pem`, privateKey)
+	}
+)
