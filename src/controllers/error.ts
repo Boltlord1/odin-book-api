1e5f2bc1cffbdd9a7cae25c78963997f1f14e40e
Boltlord1 <boltlord1@gmail.com>
2026-06-03 02:55:39 +0500

    feat: Add basic prisma error handling

--- src/controllers/error.ts
+++ src/controllers/error.ts
@@ -1,5 +1,6 @@
 import type { ErrorRequestHandler } from 'express'
-import { ClientError } from '../lib/error'
+import { PrismaClientKnownRequestError } from '../../generated/prisma/internal/prismaNamespace'
+import { ClientError, ServerError } from '../lib/error'
 import { FRONTEND_URL } from '../lib/variables'
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
@@ -16,5 +17,10 @@ export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
     if (err.name === 'profile') {
       res.redirect(`${FRONTEND_URL + path}?error=${err.msg}`)
     }
+  }
+
+  if (err instanceof PrismaClientKnownRequestError) {
+    const error = new ServerError(err.message)
+    res.json(error)
   }
 }
