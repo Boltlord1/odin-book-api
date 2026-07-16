b610d5961b39d6b4f96a6dbab49cc3fb7f89bece
Boltlord1 <boltlord1@gmail.com>
2026-07-16 11:33:08 +0500

    feat: add more rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
-import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
+import nodeExternals from 'rollup-plugin-node-externals'
 
 export default {
   input: 'src/app.ts',
   output: { file: 'dist/app.js', format: 'esm' },
-  plugins: [typescript(), commonjs()]
+  plugins: [
+    typescript(),
+    nodeExternals(),
+    nodeResolve({ extensions: ['.js', '.ts', '.json'] })
+  ]
 }
