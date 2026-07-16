fc1669382a94d5235af71a915a030a1abb0e5e32
Boltlord1 <boltlord1@gmail.com>
2026-07-16 11:24:16 +0500

    add rollup plugin for parsing cjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
+import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 
 export default {
   input: 'src/app.ts',
   output: { file: 'dist/app.js', format: 'esm' },
-  plugins: [typescript()]
+  plugins: [typescript(), commonjs()]
 }
